7e23d27b724aa287f15514530428b822a49ea9c9
Yoanny de la Rosa <havanacell840112@gmail.com>
2024-05-29 16:23:05 -0300

    Primeiro commit

---
+++ ApiCatalogo/Context/AppDbContex.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        
+        {
+
+        }
+
+    } 
+}
